13cbccca952d29d879b6e407db3ffbcf9280b926
Federico Villanueva <federubents@gmail.com>
2023-10-05 08:10:59 -0300

    estilos sencillos a la pagina

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const formulario = document.getElementById("formulario");
 
+//Label e Input Nombre
 const labelNombre = document.createElement("label");
 labelNombre.textContent = "Nombre";
+labelNombre.style.fontWeight = "bold";
 formulario.appendChild(labelNombre);
 
-//Crear input para ingreso de nombre del usuario
-const inputNombre = document.createElement("input"); //crea un elemento html con el tag <input>
-inputNombre.setAttribute("type", "text"); //asignamos atributos al nodo creado
-inputNombre.setAttribute("name", "nombreUsuario"); //se asignan los valores a cada uno de sus atributos
+const inputNombre = document.createElement("input");
+inputNombre.setAttribute("type", "text"); 
+inputNombre.setAttribute("name", "nombreUsuario");
 inputNombre.setAttribute("placeholder", "Nombre");
 formulario.appendChild(inputNombre);
 
+const saltoLineaNombre = document.createElement("br");
+formulario.appendChild(saltoLineaNombre);
+
+//Label e Input Apellido
 const labelApellido = document.createElement("label");
 labelApellido.textContent = "Apellido";
+labelApellido.style.fontWeight = "bold";
 formulario.appendChild(labelApellido);
 
 const inputApellido = document.createElement("input");
 inputApellido.setAttribute("type", "text");
-inputApellido.setAttribute("name", "nombreUsuario");
+inputApellido.setAttribute("name", "apellidoUsuario");
 inputApellido.setAttribute("placeholder", "Apellido");
 formulario.appendChild(inputApellido);
 
+const saltoLineaApellido = document.createElement("br");
+formulario.appendChild(saltoLineaApellido);
+
+//Label e Input Documento
 const numeroDocumento = document.createElement("label");
 numeroDocumento.textContent = "Numero Documento:";
+numeroDocumento.style.fontWeight = "bold";
 formulario.appendChild(numeroDocumento);
 
 const inputNumDocu = document.createElement("input");
@@ -33,13 +44,22 @@ inputNumDocu.setAttribute("step", 1);
 inputNumDocu.setAttribute("placeholder", " - ");
 formulario.appendChild(inputNumDocu);
 
+const saltoLineaDocumento = document.createElement("br");
+formulario.appendChild(saltoLineaDocumento);
+
+//Boton
+const saltoLineaBoton = document.createElement("br");
+formulario.appendChild(saltoLineaBoton);
+
 const botonEnviar = document.createElement("button");
 botonEnviar.innerHTML = "Enviar";
 formulario.appendChild(botonEnviar);
 botonEnviar.addEventListener("click", enviarDatos);
 
+
 //hacer que los datos ingresados se guarden en localStorage
 function enviarDatos() {
+
   const nombre = inputNombre.value;
   const apellido = inputApellido.value;
   const numDocumento = inputNumDocu.value;
@@ -49,19 +69,12 @@ function enviarDatos() {
     return;
   }
 
-  // Crear un objeto con los datos
-  const datosUsuario = {
-    nombre: nombre,
-    apellido: apellido,
-    numDocumento: numDocumento,
-  };
-
   // Guardar los datos en localStorage
   localStorage.setItem("nombre", nombre);
   localStorage.setItem("apellido", apellido);
   localStorage.setItem("numDocumento", numDocumento);
 
-  // Puedes también limpiar los campos del formulario si es necesario
+  // limpiar los campos del formulario si es necesario
   inputNombre.value = "";
   inputApellido.value = "";
   inputNumDocu.value = "";
@@ -69,3 +82,6 @@ function enviarDatos() {
   alert("Datos enviados correctamente y guardados.");
 }
 
+console.log(localStorage.getItem("nombre"));
+console.log(localStorage.getItem("apellido"));
+console.log(localStorage.getItem("numDocumento"));
